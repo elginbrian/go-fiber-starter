a410f75cce96c279b79c73c3525d517920c78170
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-01-02 16:13:27 +0700

    fix(domain): update the user struct

--- internal/domain/entity.go
+++ internal/domain/entity.go
@@ -1,10 +1,12 @@
 package domain
 
+import "time"
+
 type User struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"-"`
-	CreatedAt    string `json:"created"`
-	UpdatedAt    string `json:"updated"`
+    ID           int       `json:"id"`
+    Name         string    `json:"name"`
+    Email        string    `json:"email"`
+    PasswordHash string    `json:"-"`  
+    CreatedAt    time.Time `json:"created"`
+    UpdatedAt    time.Time `json:"updated"`
 }
